b7bb04d7cf6a7a7b5edba7601174b040adae1690
andryusha2002 <andrey.fedorenko2002@gmail.com>
2024-02-26 22:29:28 +0200

    first commit

---
+++ BooksController.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryappLr4
+{
+    public class BooksController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Код для отримання списку книг, наприклад з файлу конфігурації
+            // і передачі його в представлення
+
+            return View();
+        }
+    }
+}
---
+++ ValuesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryappLr4
+{
+        public class HomeController : Controller
+        {
+            public ActionResult Index()
+            {
+                return Content("Привітання з бібліотеки!");
+            }
+        }
+}
